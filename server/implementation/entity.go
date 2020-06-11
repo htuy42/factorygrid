8e0e7a3f2de1b1df87809a158f4b761c2a17d36b
Josh Pattiz <42jpattiz@gmail.com>
2020-06-11 13:08:30 -0700

    Added a mechanism for a single entity to conveniently have multiple associated tickers, which can look eachother up and interact with eachother

--- server/implementation/entity.go
+++ server/implementation/entity.go
@@ -15,18 +15,36 @@ type Entity struct{
 	// Whether or not the Entity is dead. If this is set to true, the Entity will be removed.
 	dead bool
 	// The ticker that defines specifically what type of Entity this is.
-	ticker EntityTicker
+	ticker *EntityTickerBase
 }
 
 // Provides the actual identity of a type of Entity and its control logic.
 type EntityTicker interface{
 	// Get the entities type id
 	getTypeId() int32
+	// Get the name of this EntityTicker
+	getName() string
 	// Update the Entity for a new world tick. Can read the tile, but should generally not modify it. There aren't
 	// any specific controls in place to enforce this, so its easy to break things if you want to.
 	// todo put Entity in a different package and obscure tile private members behind accessor functions so they can't
 	// be messed up as easily
 	tick(e *Entity, t *tile)
+}
+
+type EntityTickerBase struct {
+	subTickers map[string]EntityTicker
+	newSubTickers []EntityTicker
+	typeId int32
+}
+
+func (e *EntityTickerBase) tick(ent *Entity, t *tile){
+	for _,v := range e.subTickers{
+		v.tick(ent,t)
+	}
+	for _, v := range e.newSubTickers{
+		e.subTickers[v.getName()] = v
+	}
+	e.newSubTickers = []EntityTicker{}
 }
 
 // Just call the ticker, doesn't need to do anything else.
@@ -36,13 +54,16 @@ func (e *Entity) tick(t *tile){
 
 // Create an Entity at the given location with the given ticker
 func makeEntity(startX, startY int32, ticker EntityTicker) *Entity {
+	tickerBase := EntityTickerBase{}
+	tickerBase.typeId = ticker.getTypeId()
+	tickerBase.subTickers[ticker.getName()] = ticker
 	res := Entity{
 		internal: &proto.Entity{TypeId: ticker.getTypeId()},
 		worldX:   startX,
 		worldY:   startY,
 		children: []*Entity{},
 		dead:     false,
-		ticker:   ticker,
+		ticker:   &tickerBase,
 	}
 	return &res
 }
@@ -51,6 +72,10 @@ func makeEntity(startX, startY int32, ticker EntityTicker) *Entity {
 type sillyBeetle struct{}
 
 var BEETLE = &sillyBeetle{}
+
+func (s *sillyBeetle) getName() string{
+	return "sillyBeetle"
+}
 
 func (s *sillyBeetle) getTypeId() int32 {
 	return 0
